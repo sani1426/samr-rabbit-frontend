6c07ac7e701fc1b87ee113d43602741ee2ac378c
saman karimi <sam.karimi727@gmail.com>
2026-06-29 01:18:00 +0330

    hi

--- components/shared/CartDrawer.jsx
+++ components/shared/CartDrawer.jsx
@@ -11,7 +11,7 @@ const CartDrawer = () => {
   }
   return (
     <>
-      <button onClick={toggleCartDrawer} className='relative hover:text-black'>
+      <button onClick={toggleCartDrawer} className=' hover:text-black'>
         <HiOutlineShoppingBag className='w-6 h-6 text-gray-700' />
         <span className='absolute -top-1 bg-[#5b0e14] text-white text-xs rounded-full px-2 py-0.5'>
           4
