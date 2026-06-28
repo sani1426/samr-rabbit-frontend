33cf10bd235d45a27c5db79cdb412e6a261b3e37
saman karimi <sam.karimi727@gmail.com>
2026-06-29 02:58:41 +0330

    create 1 file and update 1 file

--- components/shared/CartDrawer.jsx
+++ components/shared/CartDrawer.jsx
@@ -2,12 +2,13 @@
 
 import { useMainContext } from "@/context/MainContext"
 import { IoMdClose } from "react-icons/io"
+import CartContents from "../cart/CartContents"
 
 const CartDrawer = () => {
   const { draweOpen, toggleCartDrawer } = useMainContext()
   return (
 
-    <div className={`fixed top-0 righ-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${draweOpen ? "translate-x-0" : "translate-x-full"}`}>
+    <div className={`fixed top-0 righ-0 w-3/4 sm:w-1/2 md:w-120 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${draweOpen ? "translate-x-0" : "translate-x-full"}`}>
       <div className="flex justify-end p-4">
           <button onClick={toggleCartDrawer}>
         <IoMdClose className="h-6 w-6 text-gray-600" />
@@ -15,11 +16,12 @@ const CartDrawer = () => {
       </div>
       <div className="grow p-4 overflow-y-auto">
         <h2 className="text-xl font-semibold mb-4">سبد خرید</h2>
+        <CartContents />
       </div>
 
       <div className="p-4 bg-white sticky bottom-0">
         <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition-all">پرداخت صورتحساب</button>
-        <p className="">محاسبه هزینه ارسال و وارد کردن کد تخفیف در سبد خرید</p>
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">محاسبه هزینه ارسال و وارد کردن کد تخفیف در سبد خرید</p>
       </div>
     </div>
 
