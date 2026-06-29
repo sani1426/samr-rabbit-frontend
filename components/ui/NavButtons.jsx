65ec4f79abf41130b19b829a0182691236fac043
saman karimi <sam.karimi727@gmail.com>
2026-06-29 03:42:08 +0330

    update NavButtons.jsx

--- components/ui/NavButtons.jsx
+++ components/ui/NavButtons.jsx
@@ -6,14 +6,14 @@ const NavButtons = () => {
     const { toggleCartDrawer, toggleSideBar } = useMainContext();
   return (
     <>
-      <button onClick={toggleCartDrawer} className="relative hover:text-black">
+      <button onClick={toggleCartDrawer} className="relative hover:text-black cursor-pointer">
         <HiOutlineShoppingBag className="w-6 h-6 text-gray-700" />
         <span className="absolute -top-1 bg-[#5b0e14] text-white text-xs rounded-full px-2 py-0.5">
           4
         </span>
       </button>
 
-      <button onClick={toggleSideBar} className="md:hidden">
+      <button onClick={toggleSideBar} className="md:hidden cursor-pointer">
         <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
       </button>
     </>
