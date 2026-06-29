7a05baaf4ca55a045cbb947c34e0e67891802c32
saman karimi <sam.karimi727@gmail.com>
2026-06-30 02:49:32 +0330

    update Hero.jsx

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import heroImg from "@/public/images/hero-image.png"
 
 const Hero = () => {
   return (
     <section className="">
       <Image
-        src="../../public/images/hero-image.png"
+        src={heroImg}
         alt="hero-image"
         className="w-full h-[440px] md:h-[600px] lg:h-[750px] object-cover"
       />
-      <div className="absolute inset-0  flex items-center"></div>
+      {/* <div className="absolute inset-0 bg-black  flex items-center justify-center ">
+        <div className="text-center text-white"></div> */}
+      {/* </div> */}
     </section>
   );
 };
