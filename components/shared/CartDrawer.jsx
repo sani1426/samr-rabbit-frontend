358043ac3a7f6848385116270e9d040af43505f3
saman karimi <sam.karimi727@gmail.com>
2026-06-29 03:36:33 +0330

    update CartDrawer.jsx

--- components/shared/CartDrawer.jsx
+++ components/shared/CartDrawer.jsx
@@ -34,7 +34,7 @@ const CartDrawer = () => {
       </div>
     </div>
 
-    <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${sideBarOpen ? "translate-x-full" : "translate-x-0"}`}>
+    <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${sideBarOpen ? "translate-x-0" : "translate-x-full"}`}>
       <div className="flex justify-end p-4">
         <button onClick={toggleSideBar}><IoMdClose className="h-6 w-6 text-gray-600" /></button>
       </div>
