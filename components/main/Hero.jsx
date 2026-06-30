963444ba5cbc16160f29ee703f7ac87a13fabfe6
saman karimi <sam.karimi727@gmail.com>
2026-07-01 00:29:29 +0330

    update 1 file and create 2 files

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import heroImg from "@/public/images/hero-image.png"
+import heroImg from "@/public/images/hero-1-text.png";
 
 const Hero = () => {
   return (
@@ -7,7 +7,7 @@ const Hero = () => {
       <Image
         src={heroImg}
         alt="hero-image"
-        className="w-full h-[440px] md:h-[600px] lg:h-[750px] object-cover"
+        className="w-full h-110 md:h-150 lg:h-187.5 object-cover"
       />
       {/* <div className="absolute inset-0 bg-black  flex items-center justify-center ">
         <div className="text-center text-white"></div> */}
