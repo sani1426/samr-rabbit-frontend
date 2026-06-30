e0af662bd1cb2925a044fd1614b90e69a64ed9d1
saman karimi <sam.karimi727@gmail.com>
2026-07-01 00:58:15 +0330

    update 1 file and create 1 file

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="">
+    <section className="relative">
       <Image
         src={heroImg}
         alt="hero-image"
         className="w-full h-110 md:h-150 lg:h-187.5 object-cover"
       />
-      <div className="absolute inset-0 bg-black/80  flex items-center justify-center ">
+      <div className="absolute inset-0 bg-black/95  flex items-center justify-center ">
         <div className="text-center text-white p-6">
           <h1 className="text-3xl md:text-7xl font-bold  mb-4">
             استایل خاص ,انتخاب هوشمندانه
@@ -20,7 +20,7 @@ const Hero = () => {
           </p>
           <Link
             href="#"
-            className="bg-gradient from-[#b6ff00] to-[#ffbe0b] text-white px-6 py-2 rounded-md text-lg hover:bg-transparent hover:border hover:border-[#b6ff00] hover:text-[#b6ff00] transition-all duration-400 "
+            className="bg-[#b6ff00] text-white px-6 py-2 rounded-md text-lg hover:bg-transparent hover:border hover:border-[#b6ff00] hover:text-[#b6ff00] transition-all duration-200 "
           >
             محصولات
           </Link>
