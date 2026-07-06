b644f7dbdcf09b8f4e013c83749bf48023b4611b
saman karimi <sam.karimi727@gmail.com>
2026-07-06 03:43:35 +0330

    update page.jsx

--- app/(Main)/products/[id]/page.jsx
+++ app/(Main)/products/[id]/page.jsx
@@ -1,9 +1,10 @@
+import ProductDetails from '@/components/main/ProductDetails'
 import React from 'react'
 
 const page = async ({params}) => {
     const {id} = await params
   return (
-    <div>page</div>
+    <ProductDetails />
   )
 }
 
