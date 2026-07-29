88a121b3c9515317103deaa7f2ebde95806414c9
saman karimi <sam.karimi727@gmail.com>
2026-07-29 04:14:09 +0330

    update appReducer.js

--- context/appReducer.js
+++ context/appReducer.js
@@ -6,24 +6,24 @@ import { toast } from "sonner";
 const appReducer = async (state, action) => {
   switch ((action.type)) {
     case "REGISTER_USER":{
-        const response = await axios.post(backendApi.register.url, {
+        const {data} = await axios.post(backendApi.register.url, {
           name: action.payload.name,
           email: action.payload.email,
           password: action.payload.password,
         });
   
-        if (response.data.success) {
-          localStorage.setItem("token", response.data.token);
-          localStorage.setItem("user", JSON.stringify(response.data.data));
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("user", JSON.stringify(data.data));
           toast.success("ثبت نام با موفقیت انجام شد")
           return {
             ...state,
-            user: response.data.data,
-            token: response.data.token,
+            user: data.data,
+            token: data.token,
             isAuthenticated: true,
           };
         } else {
-          console.log(response.data.message);
+          console.log(data.message);
           toast.error("ثبت نام با خطا مواجه شد")
           return {
             ...state,
