c24f83cece1de20c3c910bbcbc2484e0939aeb8a
saman karimi <sam.karimi727@gmail.com>
2026-08-04 03:22:54 +0330

    update authSlice.js

--- redux/slices/authSlice.js
+++ redux/slices/authSlice.js
@@ -93,7 +93,7 @@ const authSlice = createSlice({
             })
             .addCase(loginUser.rejected , (state , action) => {
                 state.loading = false ;
-                state.error = action.payload.message ;
+                state.error = action.payload?.message || "faild to Login" ;
             })
         
             .addCase(registerUser.pending , (state) => {
@@ -106,7 +106,7 @@ const authSlice = createSlice({
             })
             .addCase(registerUser.rejected , (state , action) => {
                 state.loading = false ;
-                state.error = action.payload.message ;
+                state.error = action.payload?.message || "faild to Register" ;
             })
         
     }
