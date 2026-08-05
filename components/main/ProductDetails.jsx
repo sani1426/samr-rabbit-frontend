c643b7747c4ead40ce7ab9529c61d6ce222480f9
saman karimi <sam.karimi727@gmail.com>
2026-08-06 02:14:25 +0330

    update ProductDetails.jsx

--- components/main/ProductDetails.jsx
+++ components/main/ProductDetails.jsx
@@ -2,147 +2,32 @@
 
 import { mapColor } from "@/utils/AppUtility";
 import ShowImage from "../ui/ShowImage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import ProductGrid from "./ProductGrid";
+import axios from "axios";
+import backendApi from "@/common/BackendApi";
+import { useParams } from "next/navigation";
 
-const selectedProduct = {
-  name: "تیشرت آستین بلند حرارتی (بافت وافلی)",
-  description:
-    "با این تیشرت حرارتی آستین بلند که از پنبه نرم با بافت وافلی ساخته شده، گرم بمانید. ایده‌آل برای پوشیدن لایه‌ای در ماه‌های سرد سال؛ طراحی جذب آن تناسب اندام راحت و گرمی را تضمین می‌کند.",
-  price: 27.99,
-  discountPrice: 22.99,
-  countInStock: 20,
-  sku: "LST-THR-009",
-  category: "Top Wear",
-  brand: "Winter Basics",
-  sizes: ["S", "M", "L", "XL", "XXL"],
-  colors: ["زغالی", "سبز", "سرمه‌ ای"],
-  collections: "Winter Essentials",
-  material: "Cotton",
-  gender: "Men",
-  images: [
-    {
-      url: "https://picsum.photos/500/500?random=7",
-      altText: "Long-Sleeve Thermal Tee Front View",
-    },
-    {
-      url: "https://picsum.photos/500/500?random=9",
-      altText: "Long-Sleeve Thermal Tee Front View",
-    },
-  ],
-  rating: 4.4,
-  numReviews: 18,
-};
-const similarProducts = [
-  {
-    _id: 1,
-    name: "شورتک جین کشی",
-    description:
-      "شورتک جین کشی راحت با فاق بلند و لبه زاپ‌دار ریش‌ریش. عالی برای ست کردن با تاپ‌های محبوب شما در ماه‌های گرم سال.",
-    price: 40,
-    discountPrice: 35,
-    countInStock: 25,
-    sku: "BW-W-008",
-    category: "Bottom Wear",
-    brand: "DenimStyle",
-    sizes: ["S", "M", "L", "XL"],
-    colors: ["آبی", "مشکی", "سفید"],
-    collections: "Denim Collection",
-    material: "Denim",
-    gender: "Women",
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=26",
-        altText: "Stretch Denim Shorts Front View",
-      },
-    ],
-    rating: 4.7,
-    numReviews: 15,
-  },
-  {
-    _id: 2,
-    name: "شلوارک دامن‌نما (کولوت)",
-    description:
-      "شلوارک گشاد مدل دامن‌نما با فاق بلند جذاب و قد برمودا (کوتاه). ترکیبی عالی از راحتی و استایل برای هر موقعیت روزمره.",
-    price: 50,
-    discountPrice: 45,
-    countInStock: 30,
-    sku: "BW-W-009",
-    category: "Bottom Wear",
-    brand: "ChicStyle",
-    sizes: ["S", "M", "L", "XL"],
-    colors: ["مشکی", "سفید", "زیتونی"],
-    collections: "Casual Collection",
-    material: "Polyester",
-    gender: "Women",
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=27",
-        altText: "Culottes Front View",
-      },
-    ],
-    rating: 4.6,
-    numReviews: 23,
-  },
-  {
-    _Id : 3,
-    name: "شلوار پارچه‌ای پیله‌دار کلاسیک",
-    description:
-      "شلوار پیله‌دار اصیل و همیشگی با دوخت ظریف و فیت. یکی از ضروریات کمد لباس برای پوشش اداری یا موقعیت‌های رسمی.",
-    price: 70,
-    discountPrice: 65,
-    countInStock: 25,
-    sku: "BW-W-010",
-    category: "Bottom Wear",
-    brand: "ElegantWear",
-    sizes: ["S", "M", "L", "XL"],
-    colors: ["سرمه‌ای", "مشکی", "خاکستری"],
-    collections: "Formal Collection",
-    material: "Wool Blend",
-    gender: "Women",
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=28",
-        altText: "Classic Pleated Trousers Front View",
-      },
-    ],
-    rating: 4.8,
-    numReviews: 20,
-  },
-  {
-    _id : 4 ,
-    name: "نیم‌تنه بافتنی کراپ",
-    description:
-      "یک نیم‌تنه بافتنی شیک و کراپ با اندامی جذب و زیبا. عالی برای ست کردن با شلوارهای جین فاق بلند یا دامن برای داشتن استایلی کژوال.",
-    price: 40,
-    discountPrice: 35,
-    countInStock: 25,
-    sku: "TW-W-001",
-    category: "Top Wear",
-    brand: "ChicKnit",
-    sizes: ["S", "M", "L"],
-    colors: ["مشکی", "سفید"],
-    collections: "Knits Collection",
-    material: "Cotton Blend",
-    gender: "Women",
-    images: [
-      {
-        url: "https://picsum.photos/500/500?random=29",
-        altText: "Knitted Cropped Top",
-      },
-    ],
-    rating: 4.6,
-    numReviews: 15,
-  },
-];
-
-const ProductDetails = () => {
+const ProductDetails =  () => {
+  const {id} = useParams()
+ const [selectedProduct , setSelectedProduct] = useState({})
+ const [similarProducts, setSimilarProducts] = useState([]);
   const [selectedSize, setSelectedSize] = useState("");
   const [selectedColor, setSelectedColor] = useState("");
   const [quantity, setQauntity] = useState(1);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-
+  const fetchData = async () => {
+    console.log(id);
+    const {data} = await axios.get(`${backendApi.getProductDetails.url}/${id}`)
+    const response = await axios.get(`${backendApi.getSimilars.url}/${id}`)
+    const result = response?.data
+    if (data?.success) setSelectedProduct(data.data)
+    if (result?.success) setSimilarProducts(result.data)
+  }
+useEffect(() => {
+fetchData()
+} ,[])
   const handleQuantityChange = (action) => {
     if (action === "plus") setQauntity((prev) => prev + 1);
     if (action === "minus" && quantity > 1) setQauntity((prev) => prev - 1);
