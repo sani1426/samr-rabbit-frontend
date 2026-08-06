1c3c730121f36555a745521435a8ae0979bb193c
saman karimi <sam.karimi727@gmail.com>
2026-08-06 03:40:54 +0330

    move 2 files and update 1 file

--- components/main/NewArrivals.jsx
+++ components/main/NewArrivals.jsx
@@ -77,7 +77,7 @@ const NewArrivals = () => {
                 draggable="false"
               />
               <div className="absolute bottom-0 right-0 left-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
-                <Link href={`/product/${item._id}`} className="block">
+                <Link href={`/products/${item._id}`} className="block">
                   <h4 className="font-medium">{item.name}</h4>
                   <p className="mt-1">${item.price}</p>
                 </Link>
