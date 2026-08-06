df8109da023c3401adb6e368f39a8491cd1323d2
saman karimi <sam.karimi727@gmail.com>
2026-08-07 02:42:01 +0330

    update page.jsx

--- app/(Main)/page.jsx
+++ app/(Main)/page.jsx
@@ -128,7 +128,7 @@ import { Suspense } from "react"
 
 const HomePage = async () => {
   let allProducts = [] ;
-  let bestProduct = {};
+  let bestProduct ;
   const {data} = await axios.get(backendApi.getAllProducts.url , {
     params : {
       gender : "Women"
@@ -137,13 +137,75 @@ const HomePage = async () => {
   if (data?.success) {
     allProducts = data?.data
   }
-  const response = await axios.get(backendApi.getBestSeller.url)
-  const result = response.data
-  if (result.success) {
-   bestProduct = result?.data
-  }else{
-    console.log(result);
+  const getBestSeller = async () => {
+    try {
+        const { data } = await axios.get(backendApi.getBestSeller.url);
+        if (data.success) {
+          bestProduct = data.data;
+        } else {
+          bestProduct =  {
+      name: "hajfhi",
+      description:
+        "این پیراهن جین راحت از پارچه جین پنبه‌ای سبک ساخته شده است. دارای تن‌خور معمولی (راسته)، دکمه‌های فشاری و لبه پایینی صاف است. با جزئیات الهام‌گرفته از استایل وسترن، این پیراهن برای پوشیدن روی تیشرت یا به تنهایی عالی است.",
+      price: 49.99,
+      discountPrice: 44.99,
+      countInStock: 15,
+      sku: "CAS-DEN-003",
+      category: "Top Wear",
+      brand: "Street Style",
+      sizes: ["S", "M", "L", "XL", "XXL"],
+      colors: ["آبی روشن", "جین تیره سنگ‌شور"],
+      collections: "Casual Wear",
+      material: "Denim",
+      gender: "Men",
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=43",
+          altText: "Casual Denim Shirt Front View",
+        },
+        {
+          url: "https://picsum.photos/500/500?random=44",
+          altText: "Casual Denim Shirt Back View",
+        },
+      ],
+      rating: 4.6,
+      numReviews: 8,
+    }
+        }
+    } catch (error) {
+      console.log(error);
+      bestProduct =  {
+      name: "پیراهن جین کژوال",
+      description:
+        "این پیراهن جین راحت از پارچه جین پنبه‌ای سبک ساخته شده است. دارای تن‌خور معمولی (راسته)، دکمه‌های فشاری و لبه پایینی صاف است. با جزئیات الهام‌گرفته از استایل وسترن، این پیراهن برای پوشیدن روی تیشرت یا به تنهایی عالی است.",
+      price: 49.99,
+      discountPrice: 44.99,
+      countInStock: 15,
+      sku: "CAS-DEN-003",
+      category: "Top Wear",
+      brand: "Street Style",
+      sizes: ["S", "M", "L", "XL", "XXL"],
+      colors: ["آبی روشن", "جین تیره سنگ‌شور"],
+      collections: "Casual Wear",
+      material: "Denim",
+      gender: "Men",
+      images: [
+        {
+          url: "https://picsum.photos/500/500?random=43",
+          altText: "Casual Denim Shirt Front View",
+        },
+        {
+          url: "https://picsum.photos/500/500?random=44",
+          altText: "Casual Denim Shirt Back View",
+        },
+      ],
+      rating: 4.6,
+      numReviews: 8,
+    }
+    }
+  
   }
+
   //   bestProduct = result.data
   //   const id = result.data._id
   //   console.log(id);
